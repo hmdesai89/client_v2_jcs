1371b9a5b12ca2aff75f6f084cf78e7836d6e2e7
Deepak Agrawal <deepacks@gmail.com>
2016-03-17 14:18:58 +0530

    add quota

--- jcs_neutron/create_neutron_req.py
+++ jcs_neutron/create_neutron_req.py
@@ -24,6 +24,22 @@ def add_params(string):
         (key, val) = p.split('=')
         params[key] = val
 
+
+def neutron_quota_service(token, tenant_id=None):
+    net_url = 'https://vpc.ind-west-1.staging.jiocloudservices.com:9696/'
+    headers = {'X-Auth-Token': token, 'User-Agent': 'curl/7.35.0'}
+
+    # neutron quota-show for a tenant
+    url_path_net_list = 'v2.0/quotas/{'+tenant_id+'}'
+    json_data = {}
+    verify=False
+    response = requests.request('GET', net_url+url_path_net_list,
+                                verify=verify,
+                                headers=headers)
+    json_response = json.loads(response.text)
+    print response
+    print json_response
+
 def create_neutron_curl_req(token):
     net_url = 'https://vpc.ind-west-1.staging.jiocloudservices.com:9696/'
     headers = {'X-Auth-Token': token, 'User-Agent': 'curl/7.35.0'}
@@ -39,10 +55,12 @@ def create_neutron_curl_req(token):
     print response
     print json_response
 
+
 def main():
     #req = sys.argv[1]
     iam_token_response = generate_iam_token_from_key()
     print iam_token_response
-    create_neutron_curl_req(iam_token_response)
+    #create_neutron_curl_req(iam_token_response)
+    neutron_quota_service(iam_token_response, '00000000000000000000961655772674')
 
 main()
